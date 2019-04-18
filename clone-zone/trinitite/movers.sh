9b4a0aef60e35e2c22b47b2cbae4852d6f530aa4
Daniel Topa <dantopa@lanl.gov>
2019-04-18 17:03:07 -0600

    modified:   clone-zone/trinitite/movers.sh

--- clone-zone/trinitite/movers.sh
+++ clone-zone/trinitite/movers.sh
@@ -4,7 +4,7 @@ printf '%s\n' "$(date) ${BASH_SOURCE[0]}"
 export path="${scratch}/spack/libraries"
 export tpls="libhio openmpi pmix ucx"
 
-for t in ${tpl}; do
-    echo 'cp *.yaml "${path}/${hpc}-standard.${t}/etc/spack/defaults/."'
-          cp *.yaml "${path}/${hpc}-standard.${t}/etc/spack/defaults/."
+for t in ${tpls}; do
+    echo 'cp *.yaml "${path}/${host_name}-standard.${t}/etc/spack/defaults/."'
+          cp *.yaml "${path}/${host_name}-standard.${t}/etc/spack/defaults/."
 done
