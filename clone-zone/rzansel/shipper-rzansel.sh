cde8e066fd6cbf8458b8de41348fb569ac7a3c91
Daniel Topa <dantopa@lanl.gov>
2019-04-30 12:08:25 -0600

    clone-zone rzansel

---
+++ clone-zone/rzansel/shipper-rzansel.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+printf '%s\n' "$(date) $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
+
+target_dir="${target_dir}"
+
+function copier(){
+    echo ""
+    echo "cp ${1}.yaml ${target_dir}"
+          cp ${1}.yaml ${target_dir}
+}
+
+function rename(){
+    echo ""
+    echo "mv ${target_dir}/${1}.yaml ${target_dir}/${2}.yaml"
+         "mv ${target_dir}/${1}.yaml ${target_dir}/${2}.yaml"
+}
+
+export list_yaml="compilers lmod-modules tcl-modules mirrors packages system-compilers"
+
+# move yaml files to $SPACK_ROOT
+for y in ${list_yaml}; do
+    copier ${c}
+done
+
+# renames
+rename tcl-modules      modules
+rename system-compilers compilers
+
+echo ""
+echo "ls -alh ${target_dir}"
+      ls -alh ${target_dir}
