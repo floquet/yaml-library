bf32b28b9023ca3ec181dfe73a58d5d58f7fce8c
Daniel Topa <dantopa@lanl.gov>
2019-05-13 15:52:48 -0600

    modified:   shell-scripts/sweepers/sweep-llvm.sh

--- shell-scripts/sweepers/sweep-llvm.sh
+++ shell-scripts/sweepers/sweep-llvm.sh
@@ -2,7 +2,7 @@
 printf '%s\n' "$(date) ${BASH_SOURCE[0]}"
 
 export tpl="llvm"
-export versions=" 7.0.0  6.0.1  6.0.0  5.0.2  5.0.1  5.0.0  4.0.1  4.0.0  3.9.1"
+export versions=" 8.0.0  7.0.1  6.0.1  5.0.2  5.0.1  4.0.1  3.9.1"
 export arch=$(spack arch)
 
 for v in ${versions}; do
