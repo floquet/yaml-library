9688fc41429affed92da4ce6551f3ef64227e3d9
Daniel Topa <dantopa@lanl.gov>
2019-06-13 08:51:57 -0600

    modified:   x-rage/capulin/sbatch-creator.sh

--- x-rage/capulin/sbatch-creator.sh
+++ x-rage/capulin/sbatch-creator.sh
@@ -13,13 +13,32 @@ SECONDS=0
 export ymd=$(date +%Y-%m-%d-%H-%M) # timestamp results
 export faust="--disable-locks"
 
-export dir_spack="capulin-arm-xrage.spack"
+export dir_spack="capulin-arm-xrage.spack" # created at run time
+export dir_xrage="/lustre/cpscratch1/dantopa/repos/github/yaml-library/x-rage" # source for
 
+export counter=0
 function new_step(){
     counter=$((counter+1))
     echo ""
     echo "Step ${counter}: ${1}"
 }
+
+new_step "Validate directories"
+if [[ ! -d "${dir_xrage}" ]]; then
+    echo "directory not found: \${dir_xrage} = ${dir_xrage}"
+    echo "looking for yaml library and bash scripts"
+    exit -1
+fi
+
+new_step "Cray module swaps"
+export spack_compiler="gcc @ 8.3.0"
+echo "spack compiler: \${spack_compiler} =  ${spack_compiler}"
+echo "module switch PrgEnv-cray PrgEnv-gnu"
+      module switch PrgEnv-cray PrgEnv-gnu
+echo "module switch cce gcc/8.3.0"
+      module switch cce gcc/8.3.0
+echo "module list"
+      module list
 
 new_step "Jump to spack directory"
 echo "cd /lustre/cpscratch1/dantopa/repos/spack/xrage"
@@ -39,8 +58,8 @@ echo "cd ${dir_spack}"
 echo ". share/spack/setup-env.sh"
       . share/spack/setup-env.sh
 
-echo "cd /lustre/cpscratch1/dantopa/repos/github/yaml-library//x-rage/capulin"
-      cd /lustre/cpscratch1/dantopa/repos/github/yaml-library//x-rage/capulin
+echo "cd ${dir_yaml}/capulin"
+      cd ${dir_yaml}/capulin
 
 # tailored yaml files
 echo "cp *.yaml ${SPACK_ROOT}/etc/spack/defaults/"
@@ -50,22 +69,46 @@ echo "cp *.yaml ${SPACK_ROOT}/etc/spack/defaults/"
 echo "cp ${SPACK_ROOT}/etc/spack/defaults/tcl-modules.yaml ${SPACK_ROOT}/etc/spack/defaults/modules.yaml"
       cp ${SPACK_ROOT}/etc/spack/defaults/tcl-modules.yaml ${SPACK_ROOT}/etc/spack/defaults/modules.yaml
 
+new_step "Load list of applications"
+list_tpl=""
+list_tpl="${list_tpl} calico@2"
+list_tpl="${list_tpl} eospac@6.4.0 eospac@6.3.1"
+list_tpl="${list_tpl} fftw@3.3.8 fftw@3.3.6-pl2"
+list_tpl="${list_tpl} googletest@1.8.1 googletest@1.7.0"
+list_tpl="${list_tpl} hdf5@1.10.5 hdf5@1.10.2"
+list_tpl="${list_tpl} hypre@2.15.1 hypre@2.11.2 hypre@2.11.1 hypre@2.10.0b"
+list_tpl="${list_tpl} kokkos@2.8.00 kokkos@2.7.24 kokkos@2.7.00"
+list_tpl="${list_tpl} libhio@1.4.1.2 libhio@1.4.1.3"
+list_tpl="${list_tpl} netlib-lapack@3.8.0 netlib-lapack@3.7.1"
+list_tpl="${list_tpl} openblas@0.3.6 openblas@0.2.20"
+list_tpl="${list_tpl} openmpi@3.0.4 openmpi@3.1.4 openmpi@4.0.1"
+list_tpl="${list_tpl} pfunit@3.3.3 pfunit@3.2.10 pfunit@3.2.9"
+list_tpl="${list_tpl} silo@4.10.2-bsd silo@4.9"
+list_tpl="${list_tpl} sundials@4.1.0 sundials@3.1.2"
+
+census=( ${list_tpl} )
+echo "${#census[@]} Spack-recognized applications to be built:"
+echo "${list_tpl}"
+
 new_step "Base installs"
 echo "cd ${SPACK_ROOT}"
       cd ${SPACK_ROOT}
 
-echo "source /lustre/cpscratch1/dantopa/repos/github/yaml-library/x-rage/bash-scripts/install-xrage-dependents.sh ${gcc_system_compiler}"
-      source /lustre/cpscratch1/dantopa/repos/github/yaml-library/x-rage/bash-scripts/install-xrage-dependents.sh ${gcc_system_compiler}
+echo "source ${dir_yaml}/bash-scripts/install-xrage-dependents.sh ${spack_compiler}"
+      source ${dir_yaml}/bash-scripts/install-xrage-dependents.sh ${spack_compiler}
 
 new_step "Secondary installs"
 echo  "No secondary installs"
 
 new_step "Verify module creation"
-echo "spack module tcl  refresh --delete-tree"
-      spack module tcl  refresh --delete-tree
+echo "spack ${faust} module tcl  refresh --delete-tree"
+      spack ${faust} module tcl  refresh --delete-tree
 
 echo "mkdir -p topa"
       mkdir -p topa
+
+echo "module list > topa/modules-list.txt"
+      module list > topa/modules-list.txt
 
 echo "module avail > topa/modules-after.txt"
       module avail > topa/modules-after.txt
@@ -78,7 +121,6 @@ done
 
 new_step "Conclude"
 echo "time used = ${SECONDS} s"
-      time used = ${SECONDS} s
 
 echo ""
 echo "mv spack-setup.out topa/spack-setup-${ymd}.out"
