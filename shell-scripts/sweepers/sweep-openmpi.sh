5520128ae51d08b6ed3e9ee9132da9d7609ac2f7
Daniel Topa <dantopa@gmail.com>
2019-07-07 12:10:47 -0600

    configs

--- shell-scripts/sweepers/sweep-openmpi.sh
+++ shell-scripts/sweepers/sweep-openmpi.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 printf '%s\n' "$(date) ${BASH_SOURCE[0]}"
 
-export tpl="openmpi"
-export versions="3.1.3 3.1.2 3.1.1 3.1.0 3.0.3 3.0.2 3.0.1 3.0.0 2.1.5 2.1.4 2.1.3 2.1.2 2.1.1 2.1.0 2.0.4 2.0.3 2.0.2 2.0.1 2.0.0"
+export compiler="gcc@7.2.0"
+export tpl="silo"
+export versions="4.10.2-bsd  4.10.2  4.9  4.8"
 export arch=$(spack arch)
 
 for v in ${versions}; do
     echo ""
-    echo "spack install ${tpl} @ ${v} % ${gcc_system_compiler} arch=${arch}"
-          spack install ${tpl} @ ${v} % ${gcc_system_compiler} arch=${arch}
+    echo "spack install ${tpl} @ ${v} % ${compiler} arch=${arch}"
+          spack install ${tpl} @ ${v} % ${compiler} arch=${arch}
 
           spack clean --all
 done
