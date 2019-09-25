b5a8d53d7119afbc3e270138d3bae25f6f0e2adc
Daniel Topa <dantopa@lanl.gov>
2019-09-25 17:38:01 -0600

    copier

--- vanguard/copier.sh
+++ vanguard/copier.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 printf '%s\n' "$(date) ${BASH_SOURCE[0]}"
 
+set -x -v
+
 cp ${host_name}/*.yaml ${SPACK_ROOT}/etc/spack/defaults
+cp ${SPACK_ROOT}/etc/spack/defaults/${host_name}-${partition}-compilers.yaml compilers.yaml
+rm ${SPACK_ROOT}/etc/spack/defaults/${host_name}-*-compilers.yaml
+
+set +x +v
