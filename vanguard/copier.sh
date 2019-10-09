e9bd66369a71ea84350ce3bb41d078782fed0495
Daniel Topa <dantopa@lanl.gov>
2019-10-08 20:20:53 -0600

    vanguard

--- vanguard/copier.sh
+++ vanguard/copier.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 printf '%s\n' "$(date) ${BASH_SOURCE[0]}"
 
-dir_from="${build_ompi}"
+dir_from="${build_ompi}/vanguard"
   dir_to="${SPACK_ROOT}/etc/spack/defaults"
 
 set -x
@@ -9,5 +9,6 @@ set -x
 # modules, packages
 cp ${dir_from}/*.yaml                      ${dir_to}
 cp ${dir_from}/${host_name}/compilers.yaml ${dir_to}
+cp ${dir_from}/${host_name}/mirrors.yaml   ${dir_to}
 
 set +x +v
